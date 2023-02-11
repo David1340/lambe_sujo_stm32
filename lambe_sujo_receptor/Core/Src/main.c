b283c2ad235c685c45917c5db431116008ebc850
David1340 <davidnike80@gmail.com>
2023-02-11 13:58:00 -0300

    Ativei o pull-up dos pinos do DIP

--- lambe_sujo_receptor/Core/Src/main.c
+++ lambe_sujo_receptor/Core/Src/main.c
@@ -544,12 +544,17 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : CE_Pin DIP1_Pin DIP2_Pin DIP3_Pin
-                           DIP4_Pin */
-  GPIO_InitStruct.Pin = CE_Pin|DIP1_Pin|DIP2_Pin|DIP3_Pin
-                          |DIP4_Pin;
+  /*Configure GPIO pin : CE_Pin */
+  GPIO_InitStruct.Pin = CE_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(CE_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DIP1_Pin DIP2_Pin DIP3_Pin DIP4_Pin */
+  GPIO_InitStruct.Pin = DIP1_Pin|DIP2_Pin|DIP3_Pin|DIP4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
